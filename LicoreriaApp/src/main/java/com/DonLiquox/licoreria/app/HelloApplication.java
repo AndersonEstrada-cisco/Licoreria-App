be09896e544243354aa638ca66d7fabc9fef47cd
unknown <a.estrada@epn.edu.ec>
2026-06-28 17:31:38 -0500

    Estructura inicial del proyecto
    Creación del proyecto con los paquetes necesario:
    - model       - controller
    - dao           - db
    - app

---
+++ LicoreriaApp/src/main/java/com/DonLiquox/licoreria/app/HelloApplication.java
@@ -0,0 +1,19 @@
+package com.DonLiquox.licoreria.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/DonLiquox/licoreria/view/hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("DonLiquox");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
---
+++ LicoreriaApp/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.DonLiquox.licoreria {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.DonLiquox.licoreria.app to javafx.fxml;
+    opens com.DonLiquox.licoreria.controller to javafx.fxml;
+    exports com.DonLiquox.licoreria.app;
+}
